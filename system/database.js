818f99ce5dc136461dd4bbef6bddca2dcbf1baea
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-04-27 12:36:32 +0700

    Update database.js

--- system/database.js
+++ system/database.js
@@ -57,11 +57,11 @@ module.exports = (m) => {
             jid: m.chat,
             activity: 0,
             autoread: true,
-            antidelete: true,
-            antilink: false,
+            antidelete: false,
+            antilink: true,
             antivirtex: false,
             filter: false,
-            left: false,
+            left: true,
             localonly: false,
             mute: false,
             member: {},
@@ -122,8 +122,8 @@ module.exports = (m) => {
          error: [],
          pluginDisable: [],
          groupmode: false,
-         sk_pack: 'Sticker',
-         sk_author: '@neoxrs',
+         sk_pack: 'Bot WhatsApp free',
+         sk_author: '@ By AnggaKz',
          self: false,
          mimic: [],
          noprefix: false,
@@ -132,12 +132,12 @@ module.exports = (m) => {
          toxic: ["ajg", "ajig", "anjas", "anjg", "anjim", "anjing", "anjrot", "anying", "asw", "autis", "babi", "bacod", "bacot", "bagong", "bajingan", "bangsad", "bangsat", "bastard", "bego", "bgsd", "biadab", "biadap", "bitch", "bngst", "bodoh", "bokep", "cocote", "coli", "colmek", "comli", "dajjal", "dancok", "dongo", "fuck", "gelay", "goblog", "goblok", "guoblog", "guoblok", "hairul", "henceut", "idiot", "itil", "jamet", "jancok", "jembut", "jingan", "kafir", "kanjut", "kanyut", "keparat", "kntl", "kontol", "lana", "loli", "lont", "lonte", "mancing", "meki", "memek", "ngentod", "ngentot", "ngewe", "ngocok", "ngtd", "njeng", "njing", "njinx", "oppai", "pantek", "pantek", "peler", "pepek", "pilat", "pler", "pornhub", "pucek", "puki", "pukimak", "redhub", "sange", "setan", "silit", "telaso", "tempek", "tete", "titit", "toket", "tolol", "tomlol", "tytyd", "wildan", "xnxx"],
          online: true,
          onlyprefix: '+',
-         owners: ['994408364923'],
+         owners: ['6281998425782'],
          lastReset: new Date * 1,
-         msg: 'Hi +tag 🪸\nI am an automated system (WhatsApp Bot) that can help to do something, search and get data / information only through WhatsApp.\n\n◦ *Database* : +db\n◦ *Library* : Baileys v+version\n◦ *Rest API* : https://api.neoxr.my.id\n◦ *Source* : https://github.com/neoxr/neoxr-bot\n\nIf you find an error or want to upgrade premium plan contact the owner.',
+         msg: 'Hi +tag 🪸\nI am an automated system (WhatsApp Bot) that can help to do something, search and get data / information only through WhatsApp.\n\n◦ *Database* : +db\n◦ *Library* : Baileys v+version\n◦ *My instagram* : https://instagram.com/anggatzi123\n◦ *Source* : https://instagram.com/anggatzi123\n\nIf you find an error or want to upgrade premium plan contact the owner.',
          menuStyle: 4,
          cover: 'https://srv.neoxr.tk/files/Ff86r7.jpg',
-         link: 'https://chat.whatsapp.com/Dqb7Z2G5mqnIPSc6xbVWuH'
+         link: 'https://chat.whatsapp.com/Ly28IB8Fel4JbPS0qCUy2p'
       }
    }
 }
